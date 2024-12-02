0733a9338e723992042b612b3070b2daa1a35f83
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2024-12-01 22:27:26 -0800

    Add files via upload

---
+++ src/assets/images/index.js
@@ -0,0 +1,19 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import microsoft from './microsoft.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+import lab from './lab.png'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    microsoft,
+    tesla,
+    logo,
+    lab
+}
